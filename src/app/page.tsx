889a9ac89d523164b14d49f15e87c02eb1ac6f19
Luke Barrier <lukebbarrier06@outlook.com>
2025-06-05 20:10:15 -0400

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default async function Page() {
     return (
         <div className="grid place-items-center w-full h-full">
             <div className="flex flex-col items-center gap-4">
-                <h1 className="text-3xl font-extrabold">Hello, create-l3-app!</h1>
+                <h1 className="text-3xl font-extrabold">safe-chat</h1>
             </div>
         </div>
     );
